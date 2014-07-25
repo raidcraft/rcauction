db54cc4de7eb8f4bbab683311c3afdad20ab0637
Dragonfire <bastian616-git@yahoo.de>
2014-07-25 16:29:56 +0200

    add constraints to avoid double bid

--- src/main/java/de/raidcraft/auction/model/TBid.java
+++ src/main/java/de/raidcraft/auction/model/TBid.java
@@ -5,14 +5,17 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.UUID;
 
 /**
  * @author Dragonfire
  */
 @Entity
-@Table(name = "auction_bids")
+@Table(name = "auction_bids", uniqueConstraints =
+@UniqueConstraint(columnNames = {"auction_id", "bid"}))
 public class TBid {
 
     @Getter
@@ -21,6 +24,7 @@ public class TBid {
     private int id;
     @Getter
     @Setter
+    @ManyToOne
     private TAuction auction;
     @Getter
     @Setter

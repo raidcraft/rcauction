21ade2298da26c059a414c218791ad3080b7ba13
Michael Reichenbach <michael@mirenoba.com>
2018-09-21 11:45:19 +0200

    fix(ebean): remove old ebean references

--- src/main/java/de/raidcraft/auction/tables/TAuction.java
+++ src/main/java/de/raidcraft/auction/tables/TAuction.java
@@ -1,15 +1,10 @@
 package de.raidcraft.auction.tables;
 
-import com.avaje.ebean.validation.NotNull;
+import io.ebean.annotation.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
